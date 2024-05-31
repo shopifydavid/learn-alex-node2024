b07fd982c8c4ebeebb3fbde7037f7dbcf4b042c0
David <david>
2024-06-01 00:09:47 +0300

    create licel folder module

---
+++ code_04/index.js
@@ -0,0 +1,7 @@
+// пример подключения именного модуля
+
+// const m1 = require('./example_module');
+// console.log(m1.randomInt());
+
+const randomNum = require('./ex_module');
+console.log(randomNum());
