d83b62b72f0875d0f4e95fc7b107afd96ace1c7d
David <david>
2024-06-01 00:08:57 +0300

    Modules | create mini project

---
+++ project/index.js
@@ -0,0 +1,4 @@
+const { randomInt, randomArr } = require('./random');
+
+console.log(randomInt(1000, 1010));
+console.log(randomArr(1000));
---
+++ project/random/index.js
@@ -0,0 +1,6 @@
+const rand = require('./random_int');
+const randomArr = require('./random_array');
+module.exports = {
+    "randomInt": rand,
+    "randomArr": randomArr
+}
